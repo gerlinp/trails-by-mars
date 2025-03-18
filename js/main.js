f6c9aa8f223f04c07afaba36dd431b45f215b1d5
Gerlin <68314210+gerlinp@users.noreply.github.com>
2025-03-18 06:05:44 -0400

    gallery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* ===================================================================
- * Spurgeon 1.0.0 - Main JS
+ * Maillard 1.0.0 - Main JS
  *
  * ------------------------------------------------------------------- */
 
@@ -21,10 +21,10 @@
         window.addEventListener('load', function() {
             html.classList.remove('ss-preload');
             html.classList.add('ss-loaded');
-
+            
             preloader.addEventListener('transitionend', function afterTransition(e) {
                 if (e.target.matches('#preloader'))  {
-                    // siteBody.classList.add('ss-show');
+                    siteBody.classList.add('ss-show');
                     e.target.style.display = 'none';
                     preloader.removeEventListener(e.type, afterTransition);
                 }
@@ -34,227 +34,163 @@
     }; // end ssPreloader
 
 
-   /* mobile menu
-    * ---------------------------------------------------- */ 
-    const ssMobileMenu = function() {
-
-        const toggleButton = document.querySelector('.s-header__menu-toggle');
-        const mainNavWrap = document.querySelector('.s-header__nav-wrap');
-        const mainNav = document.querySelector('.s-header__nav');
-        const parentMenus = mainNav.querySelectorAll('.has-children');
-        const siteBody = document.querySelector('body');
-
-        if (!(toggleButton && mainNavWrap)) return;
-
-        toggleButton.addEventListener('click', function(e) {
-            e.preventDefault();
-            toggleButton.classList.toggle('is-clicked');
-            siteBody.classList.toggle('menu-is-open');
-
-            scrollLock.getScrollState() ? scrollLock.disablePageScroll(mainNavWrap) : scrollLock.enablePageScroll(mainNavWrap);
-        });
-
-        // open (or close) submenu items in mobile view menu. 
-        // close all the other open submenu items.
-        mainNav.addEventListener('click', function(e) {
-
-            //check if the right element clicked
-            if (!e.target.closest('.has-children')) return;
-            else {
-
-                //check if element contains active class
-                if (!e.target.closest('.has-children').classList.contains('sub-menu-is-open')) {
-
-                    parentMenus.forEach(function(current) {
-                        current.classList.remove('sub-menu-is-open');
-                    });
-
-                    // add is-active class on cliked accordion
-                    e.target.closest('.has-children').classList.add('sub-menu-is-open');
-
-                } else {
-
-                    // remove is-active class on cliked accordion
-                    e.target.closest('.has-children').classList.remove('sub-menu-is-open');
-                }
-            }
-        });
-
-        window.addEventListener('resize', function() {
-
-            // above 1200px
-            if (window.matchMedia('(min-width: 1201px)').matches) {
-                if (siteBody.classList.contains('menu-is-open')) siteBody.classList.remove('menu-is-open');
-                if (toggleButton.classList.contains('is-clicked')) toggleButton.classList.remove('is-clicked');
-                if (!scrollLock.getScrollState()) scrollLock.enablePageScroll();
-
-                parentMenus.forEach(function(current) {
-                    current.classList.remove('sub-menu-is-open');
-                });
-            }
-        });
-
-    }; // end ssMobileMenu
-
-
-    /* search
+   /* offcanvas nav menu
     * ------------------------------------------------------ */
-    const ssSearch = function() {
+    const ssOffCanvas = function() {
 
-        const searchWrap = document.querySelector('.s-header__search');
-        const searchTrigger = document.querySelector('.s-header__search-trigger');
+        const menuToggle  = document.querySelector('.s-header__menu-toggle');
+        const nav         = document.querySelector('.s-header__nav');
+        const closeButton = document.querySelector('.s-header__nav-close-btn');
+        const siteBody    = document.querySelector('body');
 
-        if (!(searchWrap && searchTrigger)) return;
+        if (!(menuToggle && nav)) return;
 
-        const searchField = searchWrap.querySelector('.s-header__search-field');
-        const closeSearch = searchWrap.querySelector('.s-header__search-close');
-        const siteBody = document.querySelector('body');
-
-        searchTrigger.addEventListener('click', function(e) {
-
+        menuToggle.addEventListener('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
-            siteBody.classList.add('search-is-visible');
-
-            scrollLock.getScrollState() ? scrollLock.disablePageScroll(searchWrap) : scrollLock.enablePageScroll(searchWrap);
-
-            setTimeout(function(){
-                searchWrap.querySelector('.s-header__search-field').focus();
-            }, 100);
+            siteBody.classList.add('menu-is-open');
         });
 
-        closeSearch.addEventListener('click', function(e) {
-
+        closeButton.addEventListener('click', function(e) {
+            e.preventDefault();
             e.stopPropagation();
 
-            if(siteBody.classList.contains('search-is-visible')) {
-
-                siteBody.classList.remove('search-is-visible');
-                setTimeout(function(){
-                    searchWrap.querySelector('.s-header__search-field').blur();
-                }, 100);
-
-                scrollLock.getScrollState() ? scrollLock.disablePageScroll(searchWrap) : scrollLock.enablePageScroll(searchWrap);
+            if (siteBody.classList.contains('menu-is-open')) {
+                siteBody.classList.remove('menu-is-open');
             }
         });
 
-        searchWrap.addEventListener('click', function(e) {
-            if( !(e.target.matches('.s-header__search-inner')) ) {
-                closeSearch.dispatchEvent(new Event('click'));
+        siteBody.addEventListener('click', function(e) {
+            if(!(e.target.matches('.s-header__nav, .s-header__nav-links a'))) {
+                closeButton.dispatchEvent(new Event('click'));
             }
         });
 
-        searchField.addEventListener('click', function(e) {
-            e.stopPropagation();
-        })
-
-        searchField.setAttribute('placeholder', 'Search for...');
-        searchField.setAttribute('autocomplete', 'off');
-
-    }; // end ssSearch
+    }; // end ssOffcanvas   
 
 
-    /* masonry
-    * ------------------------------------------------------ */
-    const ssMasonry = function() {
+   /* glightbox
+    * ------------------------------------------------------ */ 
+    const ssGLightbox = function() {
 
-        const containerBricks = document.querySelector('.bricks-wrapper');
-        if (!containerBricks) return;
+        const lightbox = GLightbox({
+            selector: '.glightbox',
+            zoomable: false,
+            touchNavigation: true,
+            loop: false,
+            autoplayVideos: true,
+            svg: {
+                close: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>',
+                prev: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m9.474 5.209s-4.501 4.505-6.254 6.259c-.147.146-.22.338-.22.53s.073.384.22.53c1.752 1.754 6.252 6.257 6.252 6.257.145.145.336.217.527.217.191-.001.383-.074.53-.221.293-.293.294-.766.004-1.057l-4.976-4.976h14.692c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.692l4.978-4.979c.289-.289.287-.761-.006-1.054-.147-.147-.339-.221-.53-.221-.191-.001-.38.071-.525.215z" fill-rule="nonzero"/></svg>',
+                next: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero"/></svg>'
+            }
+        });        
 
-        imagesLoaded(containerBricks, function() {
-
-            const msnry = new Masonry(containerBricks, {
-                itemSelector: '.entry',
-                columnWidth: '.grid-sizer',
-                percentPosition: true,
-                resize: true
-            });
-
-        });
-
-    }; // end ssMasonry
-
-
-   /* animate masonry elements if in viewport
-    * ------------------------------------------------------ */
-    const ssAnimateBricks = function() {
-
-        const animateBlocks = document.querySelectorAll('[data-animate-block]');
-        const pageWrap = document.querySelector('.s-pagewrap');
-        if (!(pageWrap && animateBlocks)) return;
-
-        // on homepage do animate on scroll
-        if (pageWrap.classList.contains('ss-home')) {
-            window.addEventListener('scroll', animateOnScroll);
-        }
-        // animate on load
-        else {
-            window.addEventListener('load', function(){
-                doAnimate(animateBlocks[0]);
-            });
-        }
-
-        // do animate
-        function doAnimate(current) {
-            const els = current.querySelectorAll('[data-animate-el]');
-            const p = new Promise(function(resolve, reject) {
-
-                els.forEach(function(el, index, array) {
-                    const dly = index * 200;
-
-                    el.style.setProperty('--transition-delay', dly + 'ms');
-                    if (index === array.length -1) resolve();
-                });
-
-            });
-            
-            p.then(function() {
-                current.classList.add('ss-animated');
-            });
-        }
-
-        // animate on scroll 
-        function animateOnScroll() {
-
-            let scrollY = window.pageYOffset;
-
-            animateBlocks.forEach(function(current) {
-
-                const viewportHeight = window.innerHeight;
-                const triggerTop = (current.offsetTop + (viewportHeight * .1)) - viewportHeight;
-                const blockHeight = current.offsetHeight;
-                const blockSpace = triggerTop + blockHeight;
-                const inView = scrollY > triggerTop && scrollY <= blockSpace;
-                const isAnimated = current.classList.contains('ss-animated');
-
-                if (inView && (!isAnimated)) {
-                    doAnimate(current);
-                }
-
-            });
-        }
-
-    }; // end ssAnimateOnScroll
+    } // end ssGLightbox
 
 
    /* swiper
     * ------------------------------------------------------ */ 
     const ssSwiper = function() {
+        
+        const specialtiesSlider = function() {
 
-        const mySwiper = new Swiper('.swiper-container', {
+            const prevButton = document.querySelector('.menublock-btn-prev');
+            const nextButton = document.querySelector('.menublock-btn-next');
+            const mSlider = document.querySelector('.s-menublock__slider');            
+            if (!(mSlider)) return;
 
-            slidesPerView: 1,
-            effect: 'fade',
-            speed: 1000,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true, 
-                renderBullet: function (index, className) {
-                    return '<span class="' + className + '">' + (index + 1) + '</span>';
+            const slider = new Swiper(mSlider, {
+
+                slidesPerView: 1,
+                pagination: {
+                    // el: '.swiper-pagination',
+                    clickable: true,
+                    navigation: {
+                        nextEl: '.menublock-btn-next',
+                        prevEl: '.menublock-btn-prev',
+                    }
+                },
+                breakpoints: {
+                    // when window width is > 400px
+                    401: {
+                        slidesPerView: 1,
+                        spaceBetween: 20
+                    },
+                    // when window width is > 680px
+                    681: {
+                        slidesPerView: 2,
+                        spaceBetween: 44
+                    },
+                    // when window width is > 1100px
+                    1101: {
+                        slidesPerView: 3,
+                        spaceBetween: 50
+                    },
+                    // when window width is > 1400px
+                    1401: {
+                        slidesPerView: 3,
+                        spaceBetween: 60
+                    }
                 }
-            }
+            });
 
-        });
+            prevButton.addEventListener('click', function() {
+                if (slider.activeIndex === 0) {                    
+                  // At the beginning of the slider
+                  slider.slideTo(slider.slides.length - 1); // Go to the last slide
+                } else {
+                  slider.slidePrev();
+                }
+            });
+              
+            nextButton.addEventListener('click', function() {
+                if (slider.activeIndex === slider.slides.length - 1) {
+                  // At the end of the slider
+                  slider.slideTo(0); // Go to the first slide
+                } else {
+                  slider.slideNext();
+                }
+            });
+
+        }; // end specialtiesSlider
+
+        
+        const testimonialSlider = function() {
+
+            const tSlider = document.querySelector('.s-testimonials__slider');            
+            if (!(tSlider)) return;
+
+            const slider = new Swiper(tSlider, {
+
+                slidesPerView: 1,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    // when window width is > 400px
+                    401: {
+                        slidesPerView: 1,
+                        spaceBetween: 20
+                    },
+                    // when window width is > 900px
+                    901: {
+                        slidesPerView: 2,
+                        spaceBetween: 50
+                    },
+                    // when window width is > 1200px
+                    1201: {
+                        slidesPerView: 2,
+                        spaceBetween: 80
+                    }
+                }
+            });
+
+        }; // end testimonialSlider
+
+        specialtiesSlider();
+        testimonialSlider();
 
     }; // end ssSwiper
 
@@ -267,12 +203,12 @@
   
         boxes.forEach(function(box){
 
-            box.addEventListener('click', function(event) {
-                if (event.target.matches('.alert-box__close')) {
-                    event.stopPropagation();
-                    event.target.parentElement.classList.add('hideit');
+            box.addEventListener('click', function(e) {
+                if (e.target.matches('.alert-box__close')) {
+                    e.stopPropagation();
+                    e.target.parentElement.classList.add('hideit');
 
-                    setTimeout(function(){
+                    setTimeout(function() {
                         box.style.display = 'none';
                     }, 500)
                 }
@@ -282,32 +218,9 @@
     }; // end ssAlertBoxes
 
 
-    /* Back to Top
-    * ------------------------------------------------------ */
-    const ssBackToTop = function() {
-
-        const pxShow = 900;
-        const goTopButton = document.querySelector(".ss-go-top");
-
-        if (!goTopButton) return;
-
-        // Show or hide the button
-        if (window.scrollY >= pxShow) goTopButton.classList.add("link-is-visible");
-
-        window.addEventListener('scroll', function() {
-            if (window.scrollY >= pxShow) {
-                if(!goTopButton.classList.contains('link-is-visible')) goTopButton.classList.add("link-is-visible")
-            } else {
-                goTopButton.classList.remove("link-is-visible")
-            }
-        });
-
-    }; // end ssBackToTop
-
-
    /* smoothscroll
     * ------------------------------------------------------ */
-    const ssMoveTo = function(){
+    const ssMoveTo = function() {
 
         const easeFunctions = {
             easeInQuad: function (t, b, c, d) {
@@ -353,13 +266,10 @@
     (function ssInit() {
 
         ssPreloader();
-        ssMobileMenu();
-        ssSearch();
-        ssMasonry();
-        ssAnimateBricks();
+        ssOffCanvas();
+        ssGLightbox();
         ssSwiper();
         ssAlertBoxes();
-        ssBackToTop();
         ssMoveTo();
 
     })();
